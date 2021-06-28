dfdee1b07322e83878da080d5f1913683c70c043
Daniel Danielecki <daniel.danielecki@mail.com>
2021-06-28 08:58:05 +0200

    Add: dummy marker

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,16 +1,27 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
-import { Dimensions, StyleSheet } from 'react-native';
+import * as React from "react";
+import MapView from "react-native-maps";
+import { Dimensions, StyleSheet } from "react-native";
+import { Marker } from "react-native-maps";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
-      <MapView style={styles.map} />
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <MapView style={styles.map}>
+        <Marker
+          coordinate={{ latitude: 51.23123, longitude: 4.921321 }}
+          description="Description"
+          title="Title"
+        />
+      </MapView>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
   );
@@ -19,20 +30,20 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
