2edeb14133d5130d8077db6e0eb2ce6f0a4b68aa
Daniel Danielecki <daniel.danielecki@mail.com>
2021-07-01 15:45:15 +0200

    Add: markers from REST API

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MapView from "react-native-maps";
 import { Button, Dimensions, StyleSheet } from "react-native";
@@ -9,17 +9,46 @@ import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 
 export default function TabTwoScreen() {
+  const [markers, setMarkers] = useState(null);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "https://api.predicthq.com/v1/events/",
+      headers: {
+        Authorization: `Bearer ${PREDICTHQ_ACCESS_TOKEN}`,
+        Accept: "application/json",
+      },
+      params: {
+        category: PREDICTHQ_CATEGORIES,
+      },
+    })
+      .then((response) => {
+        setMarkers(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []); // "[]" makes sure the effect will run only once.
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       {/* TODO: Generate custom map styles based on https://mapstyle.withgoogle.com with Retro theme. */}
       <MapView style={styles.map}>
         {/* TODO: Add Callout for events fetched from API's. */}
-        <Marker
-          coordinate={{ latitude: 51.23123, longitude: 4.921321 }}
-          description="Description"
-          title="Title"
-        />
+        {markers &&
+          markers.map((marker: any, index: number) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: marker.location[1],
+                longitude: marker.location[0],
+              }}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
       </MapView>
       <View
         style={styles.separator}
@@ -28,23 +57,7 @@ export default function TabTwoScreen() {
       />
       <Button
         onPress={() => {
-          axios({
-            method: "GET",
-            url: "https://api.predicthq.com/v1/events/",
-            headers: {
-              Authorization: `Bearer ${PREDICTHQ_ACCESS_TOKEN}`,
-              Accept: "application/json",
-            },
-            params: {
-              category: PREDICTHQ_CATEGORIES,
-            },
-          })
-            .then((response) => {
-              console.log(response);
-            })
-            .catch((error) => {
-              console.log(error);
-            });
+          alert("test");
         }}
         title="Press Me"
       />
