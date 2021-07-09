1e81c05cd373da7f6ac6724153f50102365bab83
Daniel Danielecki <daniel.danielecki@mail.com>
2021-07-09 11:41:53 +0200

    Delete: not needed separator View

--- screens/SavedEventsScreen.tsx
+++ screens/SavedEventsScreen.tsx
@@ -11,11 +11,6 @@ const SavedEventsScreen = () => {
       <Text style={styles.title}>
         No saved events found. Start adding some!
       </Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
     </View>
   );
 };
@@ -46,11 +41,6 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
 
