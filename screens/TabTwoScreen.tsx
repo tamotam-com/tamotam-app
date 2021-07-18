fcb9c37514bdb40f5c337ea75b8c567d8ebc5dc7
Daniel Danielecki <daniel.danielecki@mail.com>
2021-07-18 11:18:18 +0200

    Add: sketch for toggle saved events

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,8 +1,17 @@
 import * as Localization from "expo-localization";
-import React, { useEffect, useState } from "react";
+import { toggleFavorite } from "../store/actions/events";
+import { useDispatch } from "react-redux";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import MapView from "react-native-maps";
 import { Dimensions, StyleSheet } from "react-native";
+import {
+  HeaderButton,
+  HeaderButtons,
+  HeaderButtonProps,
+  Item,
+} from "react-navigation-header-buttons";
+import { MaterialIcons } from "@expo/vector-icons";
 import { Marker } from "react-native-maps";
 import {
   PREDICTHQ_ACCESS_TOKEN,
@@ -10,6 +19,26 @@ import {
   PREDICTHQ_LIMIT,
 } from "@env";
 import { Text, View } from "../components/Themed";
+
+const MaterialHeaderButton = (
+  props: JSX.IntrinsicAttributes &
+    JSX.IntrinsicClassAttributes<HeaderButton> &
+    Readonly<HeaderButtonProps> &
+    Readonly<{ children?: React.ReactNode }>
+) => {
+  // the `props` here come from <Item ... />
+  // you may access them and pass something else to `HeaderButton` if you like
+  return (
+    <HeaderButton
+      IconComponent={MaterialIcons}
+      iconSize={23}
+      // you can customize the colors, by default colors from react navigation theme will be used
+      // color="red"
+      // pressColor="blue"
+      {...props}
+    />
+  );
+};
 
 async function onRegionChange(this: any) {
   // TODO: It breaks when the app will reload.
@@ -23,7 +52,7 @@ async function onRegionChange(this: any) {
   }
 }
 
-export default function TabTwoScreen() {
+export default function TabTwoScreen({ navigation }) {
   const [markers, setMarkers] = useState(null);
 
   useEffect(() => {
@@ -47,6 +76,33 @@ export default function TabTwoScreen() {
         console.log(error);
       });
   }, []); // "[]" makes sure the effect will run only once.
+
+  // TODO: Make adding favorites working.
+  const dispatch = useDispatch();
+
+  const toggleFavoriteHandler = useCallback(() => {
+    dispatchEvent(toggleFavorite());
+  }, [dispatch]);
+
+  // useEffect(() => {
+  //   navigation.setParam({ toggleFav: toggleFavoriteHandler });
+  // }, [toggleFavoriteHandler]);
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+          <Item
+            title="Menu"
+            iconName="ios-menu"
+            onPress={() => {
+              alert("toggle favorites");
+            }}
+          />
+        </HeaderButtons>
+      ),
+    });
+  });
 
   return (
     <View style={styles.container}>
--- store/reducers/events.tsx
+++ store/reducers/events.tsx
@@ -9,6 +9,11 @@ const eventsReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case TOGGLE_FAVORITE:
       console.log("toggle");
+      // const existingIndex = state.savedEvents.findIndex((event: any) => event.id === action.eventId);
+      const event = state.events.find(
+        (event: any) => event.id === action.eventId
+      );
+      return { ...state, savedEvents: state.savedEvents.concat(event) };
     case SET_FILTERS:
       console.log("filters");
     default:
