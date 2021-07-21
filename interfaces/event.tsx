7e983dc202417e1cf6d24f8e25ce0b6b6cda5abb
Daniel Danielecki <daniel.danielecki@mail.com>
2021-07-21 18:13:33 +0200

    Delete: commented out old code after initially having class

--- interfaces/event.tsx
+++ interfaces/event.tsx
@@ -1,26 +1,5 @@
 import { Coordinate } from "./coordinate";
 
-// class Event {
-//   id: number;
-//   coordinate: Coordinate;
-//   description: string;
-//   title: string;
-
-//   constructor(
-//     id: number,
-//     coordinate: Coordinate,
-//     description: string,
-//     title: string
-//   ) {
-//     this.id = id;
-//     this.coordinate = coordinate;
-//     this.description = description;
-//     this.title = title;
-//   }
-// }
-
-// export default Event;
-
 export interface Event {
   id: number;
   coordinate: Coordinate;
