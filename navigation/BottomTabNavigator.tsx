c7b2db77b79a019e763b607104c6ff97f38ce02f
Daniel Danielecki <daniel.danielecki@mail.com>
2021-08-02 16:24:53 +0200

    Change: FAB icon

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -67,7 +67,7 @@ export default function BottomTabNavigator() {
       </BottomTab.Navigator>
       <Portal>
         <FAB
-          icon="feather"
+          icon="map-marker-plus-outline"
           style={{
             position: "absolute",
             bottom: 100,
