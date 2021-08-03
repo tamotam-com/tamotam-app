2f5d96f6f2aad9fccaa765b1ea43d959d8333457
Daniel Danielecki <daniel.danielecki@mail.com>
2021-08-03 14:20:18 +0200

    Delete: (currently) unused Header logic

--- screens/SavedScreen.tsx
+++ screens/SavedScreen.tsx
@@ -1,96 +1,11 @@
 import StyledText from "../components/StyledText";
 import React from "react";
 import { useSelector } from "react-redux";
-import { MaterialIcons } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
-import {
-  HeaderButtons,
-  HeaderButton,
-  Item,
-  HiddenItem,
-  OverflowMenu,
-  defaultOnOverflowMenuPress,
-  HeaderButtonProps,
-} from "react-navigation-header-buttons";
 import { View } from "../components/Themed";
 
-const MaterialHeaderButton = (
-  props: JSX.IntrinsicAttributes &
-    JSX.IntrinsicClassAttributes<HeaderButton> &
-    Readonly<HeaderButtonProps> &
-    Readonly<{ children?: React.ReactNode }>
-) => {
-  // the `props` here come from <Item ... />
-  // you may access them and pass something else to `HeaderButton` if you like
-  return (
-    <HeaderButton
-      IconComponent={MaterialIcons}
-      iconSize={23}
-      // you can customize the colors, by default colors from react navigation theme will be used
-      // color="red"
-      // pressColor="blue"
-      {...props}
-    />
-  );
-};
 
-// normally, on android, text is UPPERCASED
-const ReusableCapitalizedEditItem = ({ iconName, onPress }) => {
-  return (
-    <Item
-      iconName={iconName}
-      title="back"
-      onPress={onPress}
-      buttonStyle={{ textTransform: "capitalize" }}
-    />
-  );
-};
-
-export default function SavedScreen({ navigation, route, navData }) {
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerLeft: () => (
-        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
-          <ReusableCapitalizedEditItem
-            iconName={
-              route.params && route.params.showIcon ? "arrow-back" : undefined
-            }
-            onPress={() => navigation.goBack()}
-          />
-        </HeaderButtons>
-      ),
-      // in your app, extract the arrow function into a separate component
-      // to avoid creating a new one every time
-      headerRight: () => (
-        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
-          <Item
-            title="search"
-            iconName="search"
-            onPress={() => alert("search")}
-          />
-          <OverflowMenu
-            OverflowIcon={
-              <MaterialIcons name="more-vert" size={23} color="blue" />
-            }
-            pressColor="blue"
-            onPress={(params) => {
-              defaultOnOverflowMenuPress({
-                ...params,
-                cancelButtonLabel: "cancel - custom iOS label!",
-              });
-            }}
-          >
-            <HiddenItem
-              icon={<MaterialIcons name="add" size={23} />}
-              title="add"
-              onPress={() => alert("add")}
-            />
-          </OverflowMenu>
-        </HeaderButtons>
-      ),
-    });
-  }, [navigation]);
-
+export default function SavedScreen() {
   const savedEvents = useSelector((state) => state.events.savedEvents);
 
   if (savedEvents.length === 0 || !savedEvents) {
