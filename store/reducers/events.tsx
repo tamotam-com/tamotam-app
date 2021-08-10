8a3ded853b0aaccd18a4b0d01373e87d74c326a8
Daniel Danielecki <daniel.danielecki@mail.com>
2021-08-10 13:37:56 +0200

    Refactor: adding event in reducer

--- store/reducers/events.tsx
+++ store/reducers/events.tsx
@@ -1,5 +1,6 @@
 import { EVENTS } from "../../data/dummy-data";
 import { ADD_EVENT, SET_FILTERS } from "../actions/events";
+import { Event } from "../../interfaces/event";
 
 const initialState = {
   events: EVENTS,
@@ -9,14 +10,16 @@ const initialState = {
 export default (state = initialState, action: any) => {
   switch (action.type) {
     case ADD_EVENT:
+      const newEvent: Event = {
+        id: 2,
+        coordinate: { latitude: 51.23123, longitude: 4.921321 },
+        description: "Description2",
+        title: "Title2",
+      };
+
       return {
         ...state,
-        savedEvents: state.savedEvents.concat({
-          id: 2,
-          coordinate: { latitude: 51.23123, longitude: 4.921321 },
-          description: "Description2",
-          title: "Title2",
-        }),
+        savedEvents: state.savedEvents.concat(newEvent),
       };
     case SET_FILTERS:
       console.log("filters");
