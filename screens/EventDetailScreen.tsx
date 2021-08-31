41128013b3cd3e1a93d15473a353e657d8a57aab
Daniel Danielecki <daniel.danielecki@mail.com>
2021-08-31 09:16:48 +0200

    Add: dynamic event details in EventDetailScreen.tsx

--- screens/EventDetailScreen.tsx
+++ screens/EventDetailScreen.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import StyledText from "../components/StyledText";
+import { useSelector } from "react-redux";
 import { ScrollView, StyleSheet, View } from "react-native";
 
 export default function PlaceDetailScreen() {
+  const savedEvents = useSelector((state) => state.events.savedEvents);
+
+  if (savedEvents.length === 0 || !savedEvents) {
+    return (
+      <View style={styles.content}>
+        <StyledText style={styles.title}>
+          No saved events found. Start adding some!
+        </StyledText>
+      </View>
+    );
+  }
+
   return (
     <ScrollView contentContainerStyle={{ alignItems: "center" }}>
       <View style={styles.eventDetailContainer}>
-        <StyledText>Title: Sample Title</StyledText>
-        <StyledText>Description: sample description...</StyledText>
-        <StyledText>Adress: ABC (Map Preview)</StyledText>
-        <StyledText>Adress: ABC</StyledText>
+        <StyledText>Title: {savedEvents[0].title}</StyledText>
+        <StyledText>Description: {savedEvents[0].description}</StyledText>
+        <StyledText>
+          Adress: {savedEvents[0].coordinate.latitude},{" "}
+          {savedEvents[0].coordinate.longitude}
+        </StyledText>
       </View>
     </ScrollView>
   );
@@ -21,5 +36,14 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginVertical: 20,
     padding: 20,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
