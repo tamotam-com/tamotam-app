9d384a304033bb5fba6728c0c583bf71dd4faa91
Daniel Danielecki <daniel.danielecki@mail.com>
2021-09-22 11:19:58 +0200

    Add: map in EditEventScreen.tsx

--- screens/EditEventScreen.tsx
+++ screens/EditEventScreen.tsx
@@ -2,13 +2,33 @@ import MaterialHeaderButton from "../components/MaterialHeaderButton";
 import React, { useCallback, useReducer, useState } from "react";
 import useColorScheme from "../hooks/useColorScheme";
 import Colors from "../constants/Colors";
+import MapView, { Marker } from "react-native-maps";
 import StyledText from "../components/StyledText";
 import { updateEvent } from "../store/actions/events";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, ScrollView, StyleSheet, TextInput } from "react-native";
+import {
+  Button,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+} from "react-native";
 import { Event } from "../interfaces/event";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { View } from "../components/Themed";
+import { Text, View } from "../components/Themed";
+
+// TODO: This method multiplies across components.
+async function onRegionChange(this: any) {
+  // TODO: It breaks when the app will reload.
+  if (this.mapRef) {
+    try {
+      const camera = await this.mapRef.getCamera();
+      console.log("test", camera);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
 
 export default function EditEventScreen({ navigation, route }: any) {
   const colorScheme = useColorScheme();
@@ -60,6 +80,7 @@ export default function EditEventScreen({ navigation, route }: any) {
   });
 
   const [descriptionValue, setDescriptionValue] = useState("");
+  const [selectedLocation, setSelectedLocation] = useState("");
   const [titleValue, setTitleValue] = useState("");
 
   const descriptionChangeHandler = (text: React.SetStateAction<string>) => {
@@ -84,6 +105,53 @@ export default function EditEventScreen({ navigation, route }: any) {
     navigation.goBack();
   };
 
+  const selectLocationHandler = (e: { nativeEvent: { coordinate: any } }) => {
+    const event: Event = {
+      id: 2,
+      coordinate: {
+        latitude: e.nativeEvent.coordinate.latitude,
+        longitude: e.nativeEvent.coordinate.longitude,
+      },
+      description: "dynamic test",
+      title: "dynamic title",
+    };
+    dispatch(addEvent(event));
+
+    setSelectedLocation({
+      latitude: e.nativeEvent.coordinate.latitude,
+      longitude: e.nativeEvent.coordinate.longitude,
+    });
+  };
+
+  let markerCoordinates: Coordinate = { latitude: 0, longitude: 0 };
+
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.latitude,
+      longitude: selectedLocation.longitude,
+    };
+  }
+
+  const Map = () => (
+    <View style={styles.container}>
+      <Text style={styles.title}>Map</Text>
+      {/* TODO: Generate custom map styles based on https://mapstyle.withgoogle.com with Retro theme. */}
+      <MapView
+        ref={(ref) => (this.mapRef = ref)}
+        onPress={selectLocationHandler}
+        onRegionChange={async (e) => await onRegionChange()}
+        style={styles.map}
+      >
+        {markerCoordinates && (
+          <Marker
+            title="Picked Location"
+            coordinate={markerCoordinates}
+          ></Marker>
+        )}
+      </MapView>
+    </View>
+  );
+
   return (
     <ScrollView>
       <View style={styles.form}>
@@ -101,11 +169,17 @@ export default function EditEventScreen({ navigation, route }: any) {
         />
         <Button title="Save" onPress={onSaveHandler} />
       </View>
+      <Map />
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   form: {
     margin: 30,
   },
@@ -113,11 +187,20 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 15,
   },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height / 2,
+  },
   textInput: {
     borderBottomColor: "#ccc",
     borderBottomWidth: 1,
     marginBottom: 15,
     paddingVertical: 4,
     paddingHorizontal: 2,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
