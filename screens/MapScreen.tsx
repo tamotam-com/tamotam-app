d8f3f92406fbec23fe29264c713ed6af6954dd53
Daniel Danielecki <daniel.danielecki@mail.com>
2021-12-20 09:21:00 +0100

    Fix: hardcoded image in MapScreen

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -124,10 +124,7 @@ export default function MapScreen({ navigation }: any) {
                 tooltip
               >
                 <StyledText style={styles.title}>{event.title}</StyledText>
-                <Image
-                  source={{ uri: "https://picsum.photos/700" }}
-                  style={styles.image}
-                />
+                <Image source={{ uri: event.imageUrl }} style={styles.image} />
                 <Button
                   color={
                     colorScheme === "dark"
