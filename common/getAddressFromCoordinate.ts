d4ed37f2bbabfb2faedc76d1b1654f8de8e2b083
Daniel Danielecki <daniel.danielecki@mail.com>
2022-03-08 10:06:55 +0100

    Improve: mockup for getting address from coordinates

--- common/getAddressFromCoordinate.ts
+++ common/getAddressFromCoordinate.ts
@@ -1,15 +1,17 @@
-export default async function getAddressFromCoordinate(this: any, e: any) {
+export default async function getAddressFromCoordinate(
+  mapRef: any,
+  region: any
+) {
   // TODO: Follow-up issue https://github.com/react-native-maps/react-native-maps/issues/3955 to get the correct address.
   // TODO: "this.mapRef" breaks on reloading in EditEventScreen.tsx only, but in logs is as warning in different screents. No longer the case, but it doesn't work/log anything.
-  if (this.mapRef) {
-    const { latitude, longitude } = e.nativeEvent.coordinate;
-    await this.mapRef
-      .addressForCoordinate({ latitude, longitude })
-      .then((address: any) => {
-        console.log("address", address);
-      })
-      .catch((err: Error) => {
-        console.log("err", err);
-      });
-  }
+  const { latitude, longitude } = region;
+  console.log(region);
+  await mapRef
+    .addressForCoordinate({ latitude, longitude })
+    .then((address: any) => {
+      console.log("address", address);
+    })
+    .catch((err: Error) => {
+      console.log("err", err);
+    });
 }
--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -114,7 +114,9 @@ export default function MapScreen() {
     <View style={styles.container}>
       {/* TODO: Generate custom map styles based on https://mapstyle.withgoogle.com with Retro theme. */}
       <MapView
-        onLongPress={async (e) => await getAddressFromCoordinate(e)}
+        onRegionChange={async (region) =>
+          await getAddressFromCoordinate(mapRef, region)
+        }
         ref={mapRef}
         style={styles.map}
       >
