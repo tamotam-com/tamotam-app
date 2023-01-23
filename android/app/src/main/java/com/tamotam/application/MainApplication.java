16692e6379790fffc5c0382f19a694efd2fb10f9
Daniel Danielecki <daniel.danielecki@mail.com>
2023-01-23 08:40:55 +0100

    Add: run app on iOS on Mac M1 chip

--- android/app/src/main/java/com/tamotam/application/MainApplication.java
+++ android/app/src/main/java/com/tamotam/application/MainApplication.java
@@ -19,37 +19,30 @@ import com.facebook.react.bridge.JSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import android.database.CursorWindow;
-import java.lang.reflect.Field;
 
 public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
-      this,
-      new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+    this,
+    new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for
-          // example:
-          // packages.add(new MyReactNativePackage());
-          new ReactNativeConfigPackage();
-          new AsyncStoragePackage();
-          return packages;
-        }
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
+    }
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
-      });
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  });
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -63,14 +56,6 @@ public class MainApplication extends Application implements ReactApplication {
 
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     ApplicationLifecycleDispatcher.onApplicationCreate(this);
-
-    try {
-      Field field = CursorWindow.class.getDeclaredField("sCursorWindowSize");
-      field.setAccessible(true);
-      field.set(null, 100 * 1024 * 1024); // the 100MB is the new size
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
   @Override
@@ -80,8 +65,7 @@ public class MainApplication extends Application implements ReactApplication {
   }
 
   /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method
-   * with something like
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
    * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
    *
    * @param context
@@ -92,9 +76,9 @@ public class MainApplication extends Application implements ReactApplication {
     if (BuildConfig.DEBUG) {
       try {
         /*
-         * We use reflection here to pick up the class that initializes Flipper,
-         * since Flipper library is not available in release mode
-         */
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
         Class<?> aClass = Class.forName("com.tamotam.application.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
