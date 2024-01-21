7c7fb079b668e1baa1d92d269ae32debe4cd0ae7
rnjcg <divyeshgajera2002@gmail.com>
2024-01-21 13:53:09 +0530

    Make Some improvements

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -72,6 +72,8 @@ export default function MapScreen() {
   const [selectedEndDate, setSelectedEndDate] = useState<any | Date>(new Date());
   const [SelectedDatepicker, setSelectedDatepicker] = useState<string | any>("");
   const [showDatepicker, setShowDatepicker] = useState<boolean>(false);
+  const [isStartDateSelected, setIsStartDateSelected] = useState<boolean>(false);
+  const [isEndDateSelected, setIsEndDateSelected] = useState<boolean>(false);
 
   const eventsOnMap: Event[] = isFiltering ? filteredEvents : events;
 
@@ -418,14 +420,35 @@ export default function MapScreen() {
       return;
     }
     if (SelectedDatepicker == 'end') {
-      setSelectedEndDate(selectedValueDate);
+        if (selectedValueDate && selectedValueDate < selectedStartDate) {
+          Alert.alert("Error", "End date cannot be before start date");
+          return;
+        }
+        if (selectedStartDate && selectedValueDate && isSameDay(selectedStartDate, selectedValueDate)) {
+          Alert.alert("Error", "Start and end dates cannot be the same day");
+          return;
+        }
+        else{
+          setSelectedEndDate(selectedValueDate);
+          setIsEndDateSelected(true);
+        }
+    
     } else {
       setSelectedStartDate(selectedValueDate);
+      setIsStartDateSelected(true);
     }
     setShowDatepicker(false);
     setSelectedDatepicker(''); 
   };
 
+  // Helper function to check if two dates are the same day
+    const isSameDay = (date1: Date, date2: Date): boolean => {
+      return (
+        date1.getFullYear() === date2.getFullYear() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getDate() === date2.getDate()
+      );
+    };
   const Map: () => JSX.Element = () => (
     <>
       <View style={styles.centered}>
@@ -626,28 +649,9 @@ export default function MapScreen() {
                   ? Colors.dark.background
                   : Colors.light.background,
             }]}>
-            <View style={{ marginVertical: 20, }}>
-            <Button
-              buttonColor={colorScheme === "dark" ? Colors.dark.background : Colors.light.background}
-              icon="calendar-edit"
-              mode="text"
-              onPress={() => onShowDatePicker(false)}
-              textColor={colorScheme === "dark" ? Colors.dark.text : Colors.light.text}
-              >
-              Pick start date
-            </Button>
-            <Button
-              buttonColor={colorScheme === "dark" ? Colors.dark.background : Colors.light.background}
-              icon="calendar-edit"
-              mode="text"
-              onPress={() => onShowDatePicker(true)}
-              textColor={colorScheme === "dark" ? Colors.dark.text : Colors.light.text}
-            >
-              Pick end date
-            </Button>
-            </View>
             {showDatepicker == true && (
-              <DateTimePicker
+              <DateTimePicker 
+              style={{width:"80%"}}
                 display="spinner"
                 is24Hour={true}
                 maximumDate={
@@ -663,11 +667,34 @@ export default function MapScreen() {
                 value={selectedEndDate ? selectedEndDate : selectedStartDate} />
             )}
             <View style={{ alignItems: "center", justifyContent: "center" }}>
+            <View style={{marginTop:20}}>
+              <Button
+                buttonColor={colorScheme === "dark" ? Colors.dark.background : Colors.light.background}
+                icon="calendar-edit"
+                mode="text"
+                onPress={() => onShowDatePicker(false)}
+                textColor={colorScheme === "dark" ? Colors.dark.text : Colors.light.text}
+              >
+                Pick start date
+              </Button>
+              </View>
               <StyledText>
                Start Date: {new Date(selectedStartDate).toLocaleDateString()}
               </StyledText>
             </View>
             <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <View style={{marginTop:20}}>
+            <Button
+                buttonColor={colorScheme === "dark" ? Colors.dark.background : Colors.light.background}
+                icon="calendar-edit"
+                mode="text"
+                onPress={() => onShowDatePicker(true)}
+                textColor={colorScheme === "dark" ? Colors.dark.text : Colors.light.text}
+                disabled={!isStartDateSelected} // Disable end date picker if start date is not selected
+              >
+                Pick end date
+              </Button>
+            </View>
               <StyledText>
                End Date: {new Date(selectedEndDate).toLocaleDateString()}
               </StyledText>
@@ -685,6 +712,7 @@ export default function MapScreen() {
                 textColor={
                   colorScheme === "dark" ? Colors.dark.background : Colors.light.background
                 }
+                disabled={!isStartDateSelected || !isEndDateSelected} 
               >
                 Filter
               </Button>
